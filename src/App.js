ebcf0c95911018e33808c913ef636c74beebe21a
hpc <phil0ry@aim.com>
2020-01-08 23:09:23 -0500

    got it working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Container from './Container';
+import Card from './Card';
 
 
 
@@ -8,6 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: "",
+      text: "",
       content: [
         { name: "Phil", text: "testing" },
         { name: "Xi", text: "test 2" },
@@ -15,22 +17,38 @@ class App extends Component {
     }
   }
 
-  handleSubmit = (name, text) => {
-    this.setState(prevState => {
-      const content = [ {name, text}, ...prevState.content];
-
-      return {
-        content
-      }
-    })
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    }) 
+    console.log(this.state)
   }
 
+  handleSubmit = (name, text) => {
+    let a = { name: this.state.name, text: this.state.text }
+
+    console.log(a)
+
+    this.setState({
+      content: [a, ...this.state.content],
+      name: '',
+      text: ''
+    })
+  }
 
 
   render() {
     return (
       <div className="App">
-        <Container content={this.state.content} handleSubmit={this.handleSubmit}></Container>
+        <div className="form">
+            Name: <input className="text-input" name="name" type="text" value={this.state.name} onChange={this.handleChange}></input>
+            Text: <input className="text-input" name="text" type="text" value={this.state.text} onChange={this.handleChange}></input>
+            <button onClick={this.handleSubmit}>Submit</button>
+          </div>
+  
+          {this.state.content.map((item, index) => (
+            <Card key={index} name={item['name']} text={item['text']} ></Card>
+          ))}
       </div>
     );
   }
