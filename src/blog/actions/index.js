8b95a3d8c3c0fa60afd36e621e4c7b9b5e81c648
Phil Chan <philipchanhk626@gmail.com>
2020-01-12 21:50:47 +0000

    refactor with fetchPostsAndUsers

--- src/blog/actions/index.js
+++ src/blog/actions/index.js
@@ -11,16 +11,32 @@ export const fetchPosts = () => {
     }
 };
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = (id) => {
+//     return (dispatch) => {
+//         _fetchUser(id, dispatch);
+//     };
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//         dispatch({
+//             type: 'FETCH_USER',
+//             payload: response.data
+//         })
+// });
+
+export const fetchUser = (id) => {
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
         dispatch({
             type: 'FETCH_USER',
             payload: response.data
         })
-});
+    }
+}
 
-export const fetchUser = (id) => {
-    return (dispatch) => {
-        _fetchUser(id, dispatch);
-    };
-};
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+}
