15e8cf5533dc6b29b465f6f8ddef2db51c0cbb77
Phil Chan <philipchanhk626@gmail.com>
2020-01-20 17:00:43 +0000

    reuse code to fetch users

--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import ResourceList from './ResourceList';
+import UserList from './userList';
 
 
 const Todo = () => {
@@ -8,6 +9,7 @@ const Todo = () => {
 
     return (
         <div className="ui container">
+            <UserList />
             <div>
                 <button onClick={() => setResource('posts')}>Posts</button>
                 <button onClick={() => setResource('todos')}>Todos</button>
